ec8ae60befedc1e4a4e3c0298fcb25bbf3bbd06d
orusovten <orusov22112001@mail.ru>
2020-12-14 22:48:25 +0300

    Дополнение тестов

--- Tests.py
+++ Tests.py
@@ -1,34 +1,42 @@
 import pytest
 
-from MyEarleyParser import EarlyParser
+from MyEarleyParser import EarlyParser, Situation
 
 
 def test_predict():
     rules = {'S': ['S+P', 'P'], 'P': ['P*T', 'T'], 'T': ['(S)', 'x', 'y', 'z']}
-    parser = EarlyParser(rules)
-    parser.D = [set()]
-    parser.D[0].add(("SS", "S", 0, 0))
-    parser.predict(0)
-    assert parser.D[0] == {('SS', 'S', 0, 0), ('S', 'S+P', 0, 0), ('S', 'P', 0, 0)}
+    D = [set()]
+    D[0].add(Situation("SS", "S", 0, 0))
+    EarlyParser.predict(rules, D, 0)
+    assert D[0] == {Situation('SS', 'S', 0, 0), Situation('S', 'S+P', 0, 0), Situation('S', 'P', 0, 0)}
 
 
 def test_scan():
     word = "abc"
-    parser = EarlyParser(dict())
-    parser.letters = set(word)
-    parser.D = [set(), set(), set()]
-    parser.D[1] = {("S", "bGd", 0, 1), ("T", "rbbY", 0, 1), ("T", "bb+R", 1, 0)}
-    parser.scan(word, 1)
-    assert parser.D[2] == {('T', 'bb+R', 2, 0), ('S', 'bGd', 1, 1)}
+    D = [set(), set(), set()]
+    D[1] = {Situation("S", "bGd", 0, 1), Situation("T", "rbbY", 0, 1), Situation("T", "bb+R", 1, 0)}
+    EarlyParser.scan(word, D, 1)
+    assert D[2] == {Situation('T', 'bb+R', 2, 0), Situation('S', 'bGd', 1, 1)}
 
 
 def test_complete():
-    parser = EarlyParser(dict())
-    parser.D = [set() for i in range(3)]
-    parser.D[0] = {('S', 'S+P', 0, 0), ('T', '(S)', 0, 0), ('T', 'x', 0, 0), ('P', 'T', 0, 0), ('S', 'P', 0, 0),
-                   ('T', 'y', 0, 0), ('T', 'z', 0, 0), ('P', 'P*T', 0, 0), ('SS', 'S', 0, 0)}
-    parser.D[1] = {('T', '(S)', 1, 0), ('S', 'P', 0, 1), ('T', 'y', 0, 1), ('P', 'T', 0, 1), ('T', 'z', 0, 1),
-                   ('S', 'S+P', 0, 1), ('P', 'P*T', 0, 1), ('T', '(S)', 0, 1), ('T', 'x', 0, 1)}
-    parser.D[2].add(('T', 'x', 1, 1))
-    parser.complete(2)
-    assert parser.D[2] == {('T', 'x', 1, 1), ('P', 'T', 1, 1)}
+    D = [set() for i in range(3)]
+    D[0] = {Situation('S', 'S+P', 0, 0), Situation('T', '(S)', 0, 0), Situation('T', 'x', 0, 0),
+            Situation('P', 'T', 0, 0), Situation('S', 'P', 0, 0),
+            Situation('T', 'y', 0, 0), Situation('T', 'z', 0, 0), Situation('P', 'P*T', 0, 0),
+            Situation('SS', 'S', 0, 0)}
+    D[1] = {Situation('T', '(S)', 1, 0), Situation('S', 'P', 0, 1), Situation('T', 'y', 0, 1),
+            Situation('P', 'T', 0, 1),
+            Situation('T', 'z', 0, 1),
+            Situation('S', 'S+P', 0, 1), Situation('P', 'P*T', 0, 1), Situation('T', '(S)', 0, 1),
+            Situation('T', 'x', 0, 1)}
+    D[2].add(Situation('T', 'x', 1, 1))
+    EarlyParser.complete(D, 2)
+    assert D[2] == {Situation('T', 'x', 1, 1), Situation('P', 'T', 1, 1)}
+
+
+def test_is_word_in_language():
+    rules = {"S": ["S+P", "P"], "P": ["P*T", "T"], "T": ["(S)", "x", "y", "z"]}
+    word = "(x+y)*z"
+    parser = EarlyParser(rules)
+    assert parser.is_word_in_language(word) is True
